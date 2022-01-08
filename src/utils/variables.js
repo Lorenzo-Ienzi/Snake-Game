9d8db4fa167bbb52a46625076a68e4df44ee093f
Lorenzo Ienzi <lorenzo.ienzili@gmail.com>
2022-01-08 17:55:35 +0100

    added arrows for mobile

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -7,4 +7,8 @@ export let snakeSize = canvas.width / countSquares -2
 export let gameOverText = document.getElementById("game-over")
 export let tryAgainText = document.getElementById("try-again")
 export let scoreText = document.getElementById("score")
+export let upArrowButton = document.getElementById("up")
+export let leftArrowButton = document.getElementById("left")
+export let rightArrowButton = document.getElementById("right")
+export let downArrowButton = document.getElementById("down")
  
